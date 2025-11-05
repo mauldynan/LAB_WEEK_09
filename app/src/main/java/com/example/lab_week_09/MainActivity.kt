d2c6ebb61f5e9d15ec26bbc77b6f50fd8a566c47
Maulidya Isnaini <163633669+mauldynan@users.noreply.github.com>
2025-11-05 14:36:09 +0700

    Commit No. 3 – UI Element and Theme

--- app/src/main/java/com/example/lab_week_09/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_09/MainActivity.kt
@@ -9,10 +9,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -28,6 +26,9 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.lab_week_09.ui.theme.LAB_WEEK_09Theme
+import com.example.lab_week_09.ui.theme.OnBackgroundItemText
+import com.example.lab_week_09.ui.theme.OnBackgroundTitleText
+import com.example.lab_week_09.ui.theme.PrimaryTextButton
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -83,7 +84,7 @@ fun HomeContent(
         modifier = Modifier.padding(16.dp).fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = stringResource(id = R.string.enter_item))
+        OnBackgroundTitleText(text = stringResource(id = R.string.enter_item))
 
         TextField(
             value = inputField.name,
@@ -91,25 +92,26 @@ fun HomeContent(
             onValueChange = onInputValueChange
         )
 
-        Button(
-            onClick = onButtonClick,
-            modifier = Modifier.padding(top = 8.dp)
-        ) {
-            Text(text = stringResource(id = R.string.button_click))
-        }
+        PrimaryTextButton(
+            text = stringResource(id = R.string.button_click),
+            onClick = onButtonClick
+        )
 
         LazyColumn(
-            modifier = Modifier.weight(1f).padding(top = 16.dp),
+            modifier = Modifier
+                .weight(1f)
+                .padding(top = 16.dp)
+                .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             items(listData) { item ->
-                Column (
+                Column(
                     modifier = Modifier
                         .padding(vertical = 4.dp)
                         .fillMaxSize(),
                     horizontalAlignment = Alignment.CenterHorizontally
-                ){
-                    Text(text = item.name)
+                ) {
+                    OnBackgroundItemText(text = item.name)
                 }
             }
         }
